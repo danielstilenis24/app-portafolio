efe333cbfdad72dada2364a019d4be69779fe2ec
Daniel Lenis <daniel.lenis@isoptimo.com>
2022-05-30 13:01:14 -0500

    Hasta este punto quedo hecho la intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,23 @@
-import "./intro.scss"
+import "./intro.scss";
 
 export default function Intro() {
   return (
-    <div className="intro" id="intro">
-
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src='assets/man.png' alt='' />
+        </div>
+      </div>
+      <div className='right'>
+        <div className="wrapper">
+          <h2>Hola!, yo soy</h2>
+          <h1>Daniel Lenis</h1>
+          <h3>Desarrollador <span></span></h3>
+        </div>
+        <a href="#portafolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
