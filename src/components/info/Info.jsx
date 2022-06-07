aef24016b50d17036a6c4a45abc96163dbe086c9
Daniel Lenis <daniel.lenis@isoptimo.com>
2022-06-06 22:34:23 -0500

    Corrección navbar

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,33 +1,38 @@
-import "./info.scss"
+import "./info.scss";
 
 export default function Info() {
   return (
-    <div className="info" id="info">
-        <section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-9 col-md-8 mx-auto">
-        <h1 class="fw-light">Sobre mi</h1>
-        <p class="lead text-muted">Soy desarrollador, la codificación es mi pasión y mi enfoque de mi carrera profesional. El desarrollo
-de aplicaciones para la web se ha convertido en mi impulso y motivación.
-
-Actualmente soy estudiante de la Universidad de Manizales de decimo 
-semestre del programa Ingenieria de sistemas y telecomunicaciones.
-Actualmente me encuentro laborando para una empresa llamada Isoptimo
-la cual me impulsa a aprender mas del desarrollo web, tanto en el frontend
-como en el backend.
-
-Me describo como una persona honesta, responsable,
-respetuosa, organizada, emprendedora, colaboradora, con
-liderazgo y habilidades para trabajar en equipo, creativa y proactiva
-con la disposición para aplicar los conocimientos adquiridos, así
-mismo fortalecerlos y hacer de ellos una gran utilidad para la
-empresa.</p>
-        <p>
-          <a href='assets/CV.pdf' class="btn btn-primary my-2" target='_blank'>Descargar CV</a>
-        </p>
-      </div>
+    <div className='info' id='info'>
+      <section class='py-5 text-center container'>
+        <div class='row py-lg-5'>
+          <div class='col-lg-9 col-lg-6 mx-auto'>
+            <h1 class='fw-light'>Sobre mi</h1>
+            <p class='lead text-muted mb-4'>
+              Soy desarrollador, la codificación es mi pasión y mi enfoque de mi
+              carrera profesional. El desarrollo de aplicaciones para la web se
+              ha convertido en mi impulso y motivación. Actualmente soy
+              estudiante de la Universidad de Manizales de decimo semestre del
+              programa Ingenieria de sistemas y telecomunicaciones. Actualmente
+              me encuentro laborando para una empresa llamada Isoptimo la cual
+              me impulsa a aprender mas del desarrollo web, tanto en el frontend
+              como en el backend. Me describo como una persona honesta,
+              responsable, respetuosa, organizada, emprendedora, colaboradora,
+              con liderazgo y habilidades para trabajar en equipo, creativa y
+              proactiva con la disposición para aplicar los conocimientos
+              adquiridos, así mismo fortalecerlos y hacer de ellos una gran
+              utilidad para la empresa.
+            </p>
+            <p>
+              <a
+                href='assets/CV.pdf'
+                class='btn btn-primary my-2'
+                target='_blank'>
+                Descargar CV
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
-  </section>
-    </div>
-  )
+  );
 }
