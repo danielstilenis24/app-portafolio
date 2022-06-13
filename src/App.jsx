fed5f1e4a0009fb34167f17df06fc6a2c3c0722e
Daniel Lenis <daniel.lenis@isoptimo.com>
2022-06-13 17:41:17 -0500

    Commit final para envio de proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import BarraSuperior from "./components/barraSuperior/BarraSuperior";
-import Intro from "./components/intro/Intro"
-import Info from "./components/info/Info"
-import Portafolio from "./components/portafolio/Portafolio"
-import Skills from "./components/skills/Skills"
-import Certificados from "./components/certificados/Certificados"
-import Contact from "./components/contacto/Contacto"
-import "./app.scss"
 import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./app.scss";
+
+import BarraSuperior from "./components/barraSuperior/BarraSuperior";
+import Intro from "./components/intro/Intro";
+import Info from "./components/info/Info";
+import Portafolio from "./components/portafolio/Portafolio";
+import Skills from "./components/skills/Skills";
+import Certificados from "./components/certificados/Certificados";
+import Contact from "./components/contacto/Contacto";
 import Menu from "./components/menu/Menu";
 
-
-const App = ()=> {
-  const [menuOpen,setMenuOpen]= useState(false)
+const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="app">
-    <BarraSuperior menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-    <div className="sections">
-      
-      <Intro/>
-      <Info/>
-      <Portafolio/>
-      <Skills/>
-      <Certificados/>
-      <Contact/>
-
-    
-    </div>
-    
+    <div className='app'>
+      <BarraSuperior menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className='sections'>
+        <Intro />
+        <Info />
+        <Portafolio />
+        <Skills />
+        <Certificados />
+        <Contact />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Carousel, Container } from "react-bootstrap";
 import "./skills.scss";
 
 const Skills = () => {
@@ -7,86 +7,110 @@ const Skills = () => {
       <header>
         <h1 className='h1'>SKILLS</h1>
       </header>
-      <Container>
-        <div className='d-flex justify-content-center d-flex flex-wrap'>
-          <div>
-            <img className='w-20 mx-auto' src='assets/html.png' alt='' />
-            <p className='my-4 text-center'>HTML</p>
+
+      
+        <div class='row'>
+          <div class='col'>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className='d-block w-100 img-fluid container__carrucel__img'
+                  src='https://cdn.uarizonabootcamp.com/wp-content/uploads/sites/56/2020/05/front-end-developer-skills.jpg'
+                  alt='First slide'
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className='d-block w-100 img-fluid container__carrucel__img'
+                  src='https://media.bitdegree.org/storage/media/images/2018/10/front-end-vs-back-end-developer-back-end-toolbox.png'
+                  alt='Second slide'
+                />
+              </Carousel.Item>
+            </Carousel>
           </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/css.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>CSS</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/javascript.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>JAVASCRIPT</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/react.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>REACT</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/github.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>GITHUB</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/node.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>NODE JS</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/mongo.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>MONGO DB</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/git.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>GIT</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/angular.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>ANGULAR</p>
-          </div>
-          <div>
-            <img
-              className='w-20 mx-auto'
-              src='assets/sql.png'
-              alt='HTML icon'
-            />
-            <p className='my-4 text-center'>SQL</p>
+          <div class='col'>
+            <Container>
+              <div className='d-flex justify-content-center d-flex flex-wrap'>
+                <div>
+                  <img className='w-20 mx-auto img__icons' src='assets/html.png' alt='' />
+                  <p className='my-4 text-center'>HTML</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/css.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>CSS</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/javascript.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>JAVASCRIPT</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/react.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>REACT</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/github.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>GITHUB</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/node.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>NODE JS</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/mongo.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>MONGO DB</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/git.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>GIT</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/angular.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>ANGULAR</p>
+                </div>
+                <div>
+                  <img
+                    className='w-20 mx-auto img__icons'
+                    src='assets/sql.png'
+                    alt='HTML icon'
+                  />
+                  <p className='my-4 text-center'>SQL</p>
+                </div>
+              </div>
+            </Container>
           </div>
         </div>
-      </Container>
     </div>
   );
 };
